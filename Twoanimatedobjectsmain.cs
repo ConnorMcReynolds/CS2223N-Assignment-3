79da7d7eea18b2a0f51be866a5ecd0e37d260275
Connor McReynolds <cmcreynolds1993@gmail.com>
2016-10-12 12:53:56 -0700

    Update Twoanimatedobjectsmain.cs

--- Twoanimatedobjectsmain.cs
+++ Twoanimatedobjectsmain.cs
@@ -1,26 +1,19 @@
 //Ruler:=1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
-//To the 223N class:  This program is intended to teach a few basic concepts of elementary animation.  This example uses three
-//clocks each running at different rates.  Clock rates are measured in Herz (Hz).  One clock controls the graphical refresh
-//rate.  Another clock controls the update of the coordinates of the ball traveling east.  The third clock controls the
-//coordinates of the clock traveling west.  Distinct clocks are needed because the two balls travel at different speeds. 
-
-
-//Author: F. Holliday
-//Author's email: holliday@fullerton.edu
+//Author: Connor McReynolds
+//Author's email: cmcreynolds@scu.fullerton.edu
 //Course: CPSC223N
-//Assignment number: 12
-//Due date: 07/18/2017 (mm/dd/yyyy)
-//Date last updated: 10/03/2016 (mm/dd/yyyy)
-//Source files in this program: Twoanimatedobjectsmain.cs, Twoanimatedobjectsframe.cs, Twoanimatedobjectslogic.cs
-//Purpose of this entire program:  Demonstrate how to animate two independent objects.
-//This program shows two balls moving linearly in independent directions at speeds different from each other. 
-//
-//Development status.  This program is done.  It fulfills its purpose of teaching about animation.
-//Here "program" means all three modules in the set, namely: Movingballs, Twoobjectsframe, Twoanimatedlogic
-//
-//Mame of this file: Twoanimatedobjectsmain.cs
-//Purpose of this file: Launch the window showing the form where the graphical images will be displayed area.
-//
+//Assignment number: 2
+//Due date: 10/2/2017 (mm/dd/yyyy)
+//Date last updated: 10/2/2017 (mm/dd/yyyy)
+//Source files in this program: AnimatedBallFrame.cs, AnimatedBallLogic.cs, AnimatedBallMain.cs
+//Purpose of this entire program: Create and control an animated ball
+//Development status: Almost complete, runs
+//Development platform: Bash using Mono on Xubuntu
+//Safe software seal of approval: No Microsoft products were used in the construction of this program.
+
+//Name of this file: AnimatedBallLogic
+//Purpose of this file: Launch the window showing the form where the graphical images will be displayed.
+
 //
 //The source files in this program should be compiled in the order specified below in order to satisfy dependencies.
 //  1. Twoanimatedobjectslogic.cs         compiles into library file Twoanimatedlogic.dll
@@ -31,17 +24,13 @@
 //Compile (& link) this file: 
 //mcs Twoanimatedobjectsmain.cs -r:System.Windows.Forms.dll -r:System.Drawing.dll -r:Twoobjectsframe.dll -out:Twoobjects.exe
 
-//Hardcopy: this source code is best viewed in 7 point monospaced font using portrait orientation.
-
-
-
 
 using System;
 using System.Windows.Forms;            //Needed for "Application" near the end of Main function.
-public class Movingballs
+public class Movingball
 {  public static void Main()
    {  System.Console.WriteLine("The animated ball moving program will begin now.");
-      Twoobjectsframe motionapplication = new Twoobjectsframe();
+      Animatedballframe motionapplication = new Animatedballframe();
       Application.Run(motionapplication);
       System.Console.WriteLine("This animated program has ended.  Bye.");
    }//End of Main function
